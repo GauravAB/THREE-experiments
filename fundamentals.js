0aebf0670f6e5670be071b0a2d637268b41ea811
Gaurav Ajay Bhokare <bhokare.gaurav@gmail.com>
2021-12-07 21:35:35 -0700

    instances

--- fundamentals.js
+++ fundamentals.js
@@ -15,7 +15,7 @@ function main()
 
     //set the camera
     const camera = new THREE.PerspectiveCamera(fov,aspect,near,far);
-    camera.position.z = 2;
+    camera.position.z = 3;
 
     //fix the scene with some mesh 
     const scene = new THREE.Scene();
@@ -28,7 +28,6 @@ function main()
     const material = new THREE.MeshPhongMaterial({color:0x44aa88});
     const cube = new THREE.Mesh(geometry,material);
 
-    scene.add(cube);  
  
     {
         const color = 0xFFFFFF
@@ -37,13 +36,31 @@ function main()
         light.position.set(-1,2,4);
         scene.add(light);
     }
-   
+    
+    function makeInstance(geometry, color , x)
+    {
+        const material = new THREE.MeshPhongMaterial({color});
+
+        const cube = new THREE.Mesh(geometry,material)
+        scene.add(cube);
+        cube.position.x = x;
+
+        return cube;
+    }
+
+    const cubes = [ makeInstance(geometry,0x44aa88, 0), makeInstance(geometry,0x8844aa, -2), makeInstance(geometry,0xaa8844, 2),];
+    
     function render(time)
     {
         time *= 0.001;
     
-        cube.rotation.x = time;
-        cube.rotation.y = time;
+        cubes.forEach((cube,ndx) =>{
+            const speed = 1 + ndx * .1;
+            const rot = time * speed;
+            cube.rotation.x = rot;
+            cube.rotation.y = rot;
+        });
+
         renderer.render(scene,camera);
     
         requestAnimationFrame(render);
